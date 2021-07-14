6d79365951a0a281aa1c30096ec3f87c560c6211
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2021-07-14 15:44:24 +0100

    Improved map with water slowdown, new terrain and trees

---
+++ Assets/Scenes/experiment_ctf_2hut/WaterSlowdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSlowdown : MonoBehaviour
+{
+    public float dragCoefficient;
+    public float angDragCoefficient;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb;
+        if(rb = other.GetComponent<Rigidbody>())
+        {
+            rb.drag *= dragCoefficient;
+            rb.angularDrag *= angDragCoefficient;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Rigidbody rb;
+        if (rb = other.GetComponent<Rigidbody>())
+        {
+            rb.drag /= dragCoefficient;
+            rb.angularDrag /= angDragCoefficient;
+        }
+    }
+}
