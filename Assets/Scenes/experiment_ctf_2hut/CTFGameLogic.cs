1cc432d08d64ea721ef437e0de445799dfe72b52
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2021-07-18 13:49:42 +0100

    started work on ctf game logic

---
+++ Assets/Scenes/experiment_ctf_2hut/CTFGameLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTFGameLogic : MonoBehaviour
+{
+    //flag transforms
+    public Transform redFlag;
+    public Transform blueFlag;
+
+    //flag pickup zones
+    public Collider redPickupZone;
+    public Collider bluePickupZone;
+
+    //flag capture zones
+    public Collider redCapZone;
+    public Collider blueCapZone;
+
+    //game client
+    public MultiClient client;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get game client
+        client = GameObject.FindGameObjectWithTag("GameClient").GetComponent<MultiClient>();
+
+        //setup pickup events
+    }
+
+    void PotentialPickup(Collider other)
+    {
+        if(other.GetComponent<>())
+    }
+}
